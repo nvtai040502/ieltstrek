108bc16fabaee8fd77b11ab5b94ad5c899075c00
nvtai040502 <nvtai4502@gmail.com>
2024-01-27 15:23:30 +0700

    chore(db): setup basic database for creating cambridge book manually

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
 import { PageHeader, PageHeaderDescription, PageHeaderHeading } from "@/components/page-header"
-
-const RootPage = () => {
+import axios from "axios"
+const RootPage = async () => {
+  const book = await axios.get("https://openlibrary.org/api/books?bibkeys=ISBN%3A9781108409636&format=json&jscmd=viewapi")
+  console.log("🚀 ~ RootPage ~ book:", book.data)
+  
   return (
     <div className="container relative">
       <PageHeader>
