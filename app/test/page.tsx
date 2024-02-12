46e9e635f77889746c4b4236dc13d58eac8293aa
Nguyễn Văn Tài <nvtai4502@gmail.com>
2024-02-13 01:44:15 +0700

    chore: set up prettier sort import file

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,14 @@
 'use client'
-import InlinesExample from '@/components/text-editor/inline'
+
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const App = () => {
   const a = '1'
   return (
     <div>
       <div className="h-20 w-20 whitespace-normal bg-red-500 p-0">Hello</div>
+      <Button></Button>
 
       <div>{'deed'}</div>
     </div>
